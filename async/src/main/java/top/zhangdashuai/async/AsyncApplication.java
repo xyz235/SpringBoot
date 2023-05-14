e8a34afb2459298c802b81780479a3d2ffff93d8
ZHANGDASHUAI <ZHANGDASHUAI@163.com>
2023-05-14 12:52:05 +0800

    async

---
+++ async/src/main/java/top/zhangdashuai/async/AsyncApplication.java
@@ -0,0 +1,16 @@
+package top.zhangdashuai.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangdashuai
+ */
+@SpringBootApplication
+public class AsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncApplication.class, args);
+    }
+
+}
